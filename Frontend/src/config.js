5b6acc00c239031af4b8ac1e5c03a521d2559657
ahmed soliman <ahmedsolliman28@gmail.com>
2024-04-21 14:41:54 +0200

    last Update

--- Frontend/src/config.js
+++ Frontend/src/config.js
@@ -1,2 +1,2 @@
-export const BASE_URL='http://medical-care-ikgw.vercel.app/api/v1' 
+export const BASE_URL='https://medical-care-three.vercel.app/api/v1' 
 export const token = localStorage.getItem("token")
